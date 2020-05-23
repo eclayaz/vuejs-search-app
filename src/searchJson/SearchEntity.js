7e91b7a0c6b893e344278881805e66b122cb1133
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-05-23 13:02:50 +0530

    format

--- src/searchJson/SearchEntity.js
+++ src/searchJson/SearchEntity.js
@@ -1,24 +1,36 @@
 export default async function searchEntity(entity, searchKey, searchTerm) {
   const resultShouldOnlyContains = ['_id', 'domain_names', 'name'];
-  const data = await searchInJson(entity, [{ searchKey, searchTerm }], true);
-  return Promise.resolve(cleanup(data, resultShouldOnlyContains));
+  const data = await searchInJson(
+    entity,
+    [{ searchKey, searchTerm }],
+    true,
+    resultShouldOnlyContains
+  );
+
+  return Promise.resolve(data);
 }
 
-const searchInJson = async function(entity, searchCriteria, multiple) {
+const searchInJson = async function(
+  entity,
+  searchCriteria,
+  multiple,
+  resultShouldOnlyContains
+) {
   try {
     const data = await readFile(entity);
-
+    let result;
     if (multiple) {
-      const result = data.filter((item) => {
+      result = data.filter((item) => {
         return isMatchingWithCriteria(item, searchCriteria);
       });
-      return Promise.resolve(result);
+    } else {
+      result = data.find((item) => {
+        return isMatchingWithCriteria(item, searchCriteria);
+      });
+      result = [result];
     }
 
-    const result = data.find((item) => {
-      return isMatchingWithCriteria(item, searchCriteria);
-    });
-    return Promise.resolve([result]);
+    return Promise.resolve(cleanup(result, resultShouldOnlyContains));
   } catch (err) {
     return Promise.reject(err);
   }
